804e96d7b3920df774accc98d9d1219bec5c4f53
Ryan Plain <ryan@Ryans-MBP.local>
2024-08-01 08:20:59 -0600

    fix spacing

--- R/helpers2.R
+++ R/helpers2.R
@@ -85,21 +85,17 @@ get_base_table <- function(adp, draft_picks) {
     value = draft_position - adp,
     target = case_when(
       merge_name %in% c(
-        "jerry jeudy"
+        "jerry jeudy",
+        "tyreek hill"
       ) ~ "Avoid",
       merge_name %in% c(
-        "tank bigsby"
+        "tank bigsby",
+        'ceedee lamb'
       ) ~ "Target",
       TRUE ~ ""
     )
   ) |>
   #mutate(position = ifelse(is.na(position), substr(pos, 1, 2), position))
-  rowwise() |>
-  mutate(
-    upper = floor(min(underdog, nffc, rt, yahoo, adp)),
-    lower = ceiling(max(underdog, nffc, rt, yahoo, adp))
-  ) |>
-  ungroup() |>
   arrange(adp) |>
   mutate(rank = row_number()) |>
   group_by(position) |>
@@ -155,3 +151,197 @@ get_base_table <- function(adp, draft_picks) {
 
 
 }
+
+
+clean_base_table <- function(df_base) {
+
+  df_base |>
+    rowwise() |>
+    mutate(
+      across(c(rt, underdog, nffc, yahoo, adp), \(x) if_else(is.na(x), 999, x)),
+      upper = floor(min(underdog, nffc, rt, yahoo, adp)),
+      lower = ceiling(max(underdog, nffc, rt, yahoo, adp))
+    ) |>
+    ungroup() |>
+    group_by(position) |>
+    arrange(adp) |>
+    mutate(
+      pos_rank = row_number(),
+
+      )
+
+}
+
+
+draft_picks <- get_draft_picks()
+df_base <- get_base_table(clean_fantasy_life_adp('~/Downloads/nfl_adp.csv', 2024), draft_picks)
+df_base
+
+
+df_base <- df_base |> mutate(drafted = ifelse(merge_name %in% c('ceedee lamb', 'tyreek hill', 'christian mccaffrey'), FALSE, drafted))
+
+library(reactable)
+
+
+orange_pal <- function(x) rgb(colorRamp(c("#ffe4aa", "#ff9500"))(x), maxColorValue = 255)
+blue_pal <- function(x) rgb(colorRamp(c("#aae4ff", "#0095ff"))(x), maxColorValue = 255)
+red_pal <- function(x) rgb(colorRamp(c("#ffaae4", "#ff1100"))(x), maxColorValue = 255)
+blue_pal <- function(x) rgb(colorRamp(c("#aae4ff", "#0095ff"))(x), maxColorValue = 255)
+BuYlRd <- function(x) rgb(colorRamp(c("#7fb7d7", "#ffffbf", "#fc8d59", bias = 3))(x), maxColorValue = 255)
+BuYlRd <- function(x) rgb(colorRamp(c("#000000","#004949","#009292","#ff6db6","#ffb6db",
+                                      "#490092","#006ddb","#b66dff","#6db6ff","#b6dbff",
+                                      "#920000","#924900","#db6d00","#24ff24","#ffff6d"))(x), maxColorValue = 255)
+
+
+
+reactable(
+  df_base
+)
+
+
+
+
+
+df_base |>
+  relocate(url, .after = position) |>
+  relocate(team_logo_wikipedia, .after = team) |>
+  relocate(target, .after = adp_formatted) |>
+  relocate(rank, .before = player) |>
+  reactable(
+    style = list(fontSize = "17px"),
+    defaultPageSize = 10,
+    striped = TRUE,
+    highlight = TRUE,
+    searchable = TRUE,
+    columns = list(
+      # avg_adp = colDef(
+      #   name = "Value",
+      #   format = colFormat(digits = 1),
+      #   style = function(x) {
+      #     if(is.na(x)) return()
+      #     if (x > 0) {
+      #       color <- make_color_pallete(viridisLite::viridis(20))(1/x)
+      #     } else if (x <= 0) {
+      #       color <- 'red'
+      #     } else {
+      #       color <- 'blue'
+      #     }
+      #     list(background = color)
+      #   }
+      # ),
+      # ud_adp_diff = colDef(
+      #   name = "UD ADP Diff",
+      #   format = colFormat(digits = 1),
+      #   style = list(position = 'sticky', left = 0),
+      #   headerStyle = list(position = 'sticky', left = 0)
+      # ),
+      # stdev = colDef(
+      #   name = "Variability",
+      #   # Render the bar charts using a custom cell render function
+      #   cell = function(value) {
+      #     width <- value
+      #     bar_chart(value, width = width, fill = "#3fc1c9")
+      #   },
+      #   # And left-align the columns
+      #   align = "left",
+      #   style = list(position = 'sticky', left = 0),
+      #   headerStyle = list(position = 'sticky', left = 0)
+      # ),
+      rank = colDef(
+        name = "ADP",
+        style = function(value, index) {
+          if (df_base$drafted[index] == TRUE) {
+            color <- "grey"
+          } else if (df_base$position[index] == "RB") {
+            color <- "#8ff2cacc"
+          } else if (df_base$position[index] == "WR") {
+            color <- "#56c9f8cc"
+          } else if (df_base$position[index] == "QB") {
+            color <- "#ef74a1cc"
+          } else if (df_base$position[index] == "TE") {
+            color <- "#feae58cc"
+          }
+          list(background = color, color = 'black')
+        },
+
+      ),
+      position = colDef(
+        name = "Position",
+        align = 'center',
+        style = function(value, index) {
+          if (df_base$drafted[index] == TRUE) {
+            color <- "grey"
+          } else if (df_base$position[index] == "RB") {
+            color <- "#8ff2cacc"
+          } else if (df_base$position[index] == "WR") {
+            color <- "#56c9f8cc"
+          } else if (df_base$position[index] == "QB") {
+            color <- "#ef74a1cc"
+          } else if (df_base$position[index] == "TE") {
+            color <- "#feae58cc"
+          }
+          list(background = color, color = 'black')
+        }
+      ),
+      player = colDef(
+        name = "Player",
+        style = function(value, index) {
+          if (df_base$drafted[index] == TRUE) {
+            color <- "grey"
+          } else if (df_base$position[index] == "WR") {
+            color <- "#56c9f8cc"
+          } else if (df_base$position[index] == "QB") {
+            color <- "#ef74a1cc"
+          } else if (df_base$position[index] == "TE") {
+            color <- "#feae58cc"
+          } else if (df_base$position[index] == "RB") {
+            color <- "#8ff2cacc"
+          }
+          list(background = color, color = 'black')
+        }
+      ),
+      url = colDef(
+        name = 'Headshot',
+        cell = reactablefmtr::embed_img(height = 166/2.8, width = 250/3),
+        style = function(value, index) {
+          if(df_base$drafted[index] == TRUE) color <- 'grey'
+          else color <- NULL
+          list(background = color)
+        }
+      ),
+      team_logo_wikipedia   = colDef(
+        name = 'Team',
+        cell = reactablefmtr::embed_img(height = 50, width = 75),
+        style = function(value, index) {
+          if(df_base$drafted[index] == TRUE) color <- 'grey'
+          else color <- df_base$team_color[index]
+          list(background = color)
+        }
+      ),
+      target = colDef(
+        name = 'Target',
+        align = 'center',
+        cell = function(value) {
+          if (value  == 'Avoid' ) {
+            shiny::icon("warning", class = "fas fa-3x", style = "color: red")
+          } else if (value == 'Target') {
+            shiny::icon("thumbs-up", class = 'fas fa-3x', style = "color: cyan")
+          } else value
+        }
+      ),
+      round = colDef(
+        name = 'Round',
+        align = 'center',
+        style = function(value, index) {
+          #if(!is.numeric(value)) return()
+          normalized <- (df_base$round[index] - min(df_base$round)) / (max(df_base$round) - min(df_base$round))
+          color <- BuYlRd(normalized)
+          list(background = color)
+        }
+      )
+
+
+      )
+
+
+    )
