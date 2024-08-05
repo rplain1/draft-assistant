ef4eebd17c34d3b9f02bdedaae9482a434bded00
Ryan Plain <ryan@Ryans-MBP.local>
2024-08-05 07:59:49 -0600

    adding ffc adp

--- R/helpers2.R
+++ R/helpers2.R
@@ -1,9 +1,19 @@
+library(httr)
+library(jsonlite)
+library(tidyverse)
+
+TEAMS <- nflreadr::load_teams()
+TARGETS <- c('jaleel mclaughlin')
+AVOID <- c('javonte williams')
+
+
+
 # TODO: fix this to be dynamic to league and year
-get_draft_picks <- function() {
-  league_id <- '996102938634387456'
+get_draft_picks <- function(league_id = '996102938634387456', year = 2024) {
+  league_id <- league_id
 
   my_league <- ffscrapr::ff_connect(platform = 'sleeper',
-                                    season = 2023,
+                                    season = year,
                                     league_id = league_id)
 
 
@@ -13,9 +23,9 @@ get_draft_picks <- function() {
 }
 
 # TODO: Fix this to be dynamic to league size
-get_pick_number <- function(draft_picks) {
-  if(nrow(draft_picks) <= 12) 1
-  else(nrow(draft_picks[12:nrow(draft_picks), ]))
+get_pick_number <- function(draft_picks, team_size = 10) {
+  if(nrow(draft_picks) <= team_size) 1
+  else(nrow(draft_picks[team_size:nrow(draft_picks), ]))
 }
 
 get_round <- function(pick_number) ceiling(pick_number/12)
@@ -43,7 +53,7 @@ clean_fantasy_life_adp <- function(path, season = ffl::most_recent_season()) {
   merge_name = ffscrapr::dp_clean_names(player_name = player, lowercase = TRUE, convert_lastfirst = TRUE, use_name_database = TRUE)
   )
 
-  players <- ffscrapr::dp_playerids() |>
+  players <- nflreadr::load_ff_playerids() |>
     dplyr::filter(db_season == season, !is.na(sleeper_id)) |>
     filter(sleeper_id != '748') # wrong mike williams
 
@@ -54,103 +64,24 @@ clean_fantasy_life_adp <- function(path, season = ffl::most_recent_season()) {
    ) |>
     select(player,
       team = team.x, merge_name, position, underdog
-      , rt, nffc, yahoo, adp, sleeper_id
+      , rt, nffc, yahoo, adp, sleeper_id, adp
     )
 
-  empty_rows <- nrow(joined_table[ is.na(joined_table$sleeper_id), ])
+  empty_rows <- nrow(joined_table[ is.na(joined_table$sleeper_id) & joined_table$position %in% c('RB', 'WR','TE', 'QB'), ])
 
   if(empty_rows != 0) {
     message(glue::glue("{empty_rows} did not join"))
   }
-  message("All players identified!")
+  else {
+    message("All players identified!")
+  }
+  
 
   return(joined_table)
 
 }
 
-get_base_table <- function(adp, draft_picks) {
 
-  teams <- nflreadr::load_teams()
-
-  adp |>
-  left_join(
-    draft_picks |>
-      #mutate(draft_position = (round * max(pick)) + pick) |>
-      mutate(draft_position = row_number()) |>
-      select(player_id, draft_position),
-    by = c("sleeper_id" = "player_id")
-  ) |>
-  mutate(
-    drafted = !is.na(draft_position),
-    value = draft_position - adp,
-    target = case_when(
-      merge_name %in% c(
-        "jerry jeudy",
-        "tyreek hill"
-      ) ~ "Avoid",
-      merge_name %in% c(
-        "tank bigsby",
-        'ceedee lamb'
-      ) ~ "Target",
-      TRUE ~ ""
-    )
-  ) |>
-  #mutate(position = ifelse(is.na(position), substr(pos, 1, 2), position))
-  arrange(adp) |>
-  mutate(rank = row_number()) |>
-  group_by(position) |>
-  mutate(
-    tier = round(
-      lead(ifelse(drafted == FALSE & target != "Avoid", adp,
-        ifelse(drafted == FALSE & target == "Avoid", lead(adp),
-          lead(adp, 2)
-        )
-      )) - (adp)
-    ),
-    tier = ifelse(drafted == TRUE, NA, tier),
-    round_ = ceiling(rank / 12)
-  ) |>
-  ungroup() |>
-  group_by(round_) |>
-  mutate(
-    pick = row_number(),
-    pick = as.character(pick),
-    pick = ifelse(nchar(pick) == 1, paste0("0", pick), pick)
-  ) |>
-  ungroup() |>
-  mutate(adp_formatted = paste(round_, pick, sep = ".")) |>
-  select(-round_, -pick) |>
-  relocate(position, .before = team) |>
-  mutate(
-    color_ = case_when(
-      drafted == TRUE ~ "grey",
-      position == "TE" ~ "#feae58cc",
-      position == "QB" ~ "#ef74a1cc",
-      position == "RB" ~ "#8ff2cacc",
-      position == "WR" ~ "#56c9f8cc",
-      TRUE ~ "black"
-    )
-  ) |>
-  left_join(
-    teams |>
-      mutate(team_abbr = ifelse(team_abbr == "LA", "LAR", team_abbr)) |>
-      select(team_abbr, ends_with("color")),
-    by = c("team" = "team_abbr")
-  ) |>
-  head(300) |>
-  mutate(
-    url = glue::glue(
-      "https://sleepercdn.com/content/nfl/players/thumb/{sleeper_id}.jpg"
-    ),
-    round = get_round(rank)
-  ) |>
-  left_join(
-    teams |> select(team = team_abbr, team_logo_wikipedia),
-    by = "team"
-  )
-
-
-}
 
 
 clean_base_table <- function(df_base) {
@@ -173,9 +104,141 @@ clean_base_table <- function(df_base) {
 }
 
 
+
+
+
+get_ffc_adp <- function(format, year, teams = "12", position = "all") {
+  url <- glue::glue("https://fantasyfootballcalculator.com/api/v1/adp/{format}?teams={teams}&year={year}&position={position}")
+  # Fetch the data from the URL
+  response <- GET(url)
+  
+  # Check if the request was successful
+  if (status_code(response) == 200) {
+    # Parse the JSON content
+    data <- content(response, as = "text")
+    json_data <- fromJSON(data, flatten = TRUE)
+  } else {
+    cat("Error: ", status_code(response), "\n")
+  }
+  
+  json_data$players |>
+    as_tibble() |>
+    mutate(season = year)
+  
+}
+
+clean_ff_ids <- function(ff_ids) {
+  
+  ff_ids |> filter(is.na(
+    case_when(
+      merge_name == "mike williams" & gsis_id == "00-0033536" ~ TRUE,
+      merge_name == "steve smith" & gsis_id == "00-0020337" ~ TRUE,
+      merge_name == "zach miller" & gsis_id == "00-0027125" ~ TRUE,
+      TRUE ~ NA)
+  )
+  )
+}
+
+ff_ids <- nflreadr::load_ff_playerids() |> 
+  clean_ff_ids()
+
+player_stats <- nflreadr::load_player_stats(seasons = 2023, stat_type = c('offense'))
+
+
+clean_ffc_adp <- function(ffc_adp, ff_ids) {
+
+  ffc_adp$join_name <- nflreadr::clean_player_names(ffc_adp$name)
+  ffc_adp |>
+    mutate(
+      join_name = case_when(
+        join_name == "Mike Vick" ~ "Michael Vick",
+        str_detect(join_name, "Jr") ~ str_remove(join_name, " Jr"),
+        TRUE ~ join_name
+      ),
+      join_name = str_to_lower(join_name)
+    ) |>
+    inner_join(ff_ids |>
+                 select(gsis_id, merge_name, sleeper_id, position, pfr_id) |>
+                 filter(!is.na(gsis_id)), by = c("join_name" = "merge_name", "position" = "position")) |>
+    group_by(season, position) |>
+    arrange(adp) |>
+    mutate(pos_rank = row_number()) |>
+    ungroup()
+
+}
+
+
+clean_base_table <- function(adp, draft_picks) {
+  
+  adp |>
+    left_join(
+      draft_picks |>
+        #mutate(draft_position = (round * max(pick)) + pick) |>
+        mutate(draft_position = row_number()) |>
+        select(player_id, draft_position),
+      by = c("sleeper_id" = "player_id")
+    ) |>
+    mutate(
+      drafted = !is.na(draft_position),
+      value = draft_position - adp,
+      # TODO look at using sleeper ids
+      target = case_when(
+        join_name %in% AVOID ~ "Avoid",
+        join_name %in% TARGETS ~ "Target",
+        TRUE ~ ""
+      )
+    ) |>
+  relocate(position, .before = team) |>
+    mutate(
+      color_ = case_when(
+        drafted == TRUE ~ "grey",
+        position == "TE" ~ "#feae58cc",
+        position == "QB" ~ "#ef74a1cc",
+        position == "RB" ~ "#8ff2cacc",
+        position == "WR" ~ "#56c9f8cc",
+        TRUE ~ "black"
+      )
+    ) |>
+    left_join(
+      TEAMS |>
+        mutate(team_abbr = ifelse(team_abbr == "LA", "LAR", team_abbr)) |>
+        select(team_abbr, ends_with("color")),
+      by = c("team" = "team_abbr")
+    ) |>
+    mutate(
+      url = glue::glue(
+        "https://sleepercdn.com/content/nfl/players/thumb/{sleeper_id}.jpg"
+      ),
+      round = map(.x = draft_position, get_round)
+    ) |>
+    left_join(
+      teams |> select(team = team_abbr, team_logo_wikipedia),
+      by = "team"
+    )
+  
+  
+}
+
+combine_ffc_fantasy_life_adp <- function(df_ffc, df_fantasy_life) {
+  df_ffc |> 
+    left_join(
+      df_fantasy_life |> 
+        select(-player, -team, -merge_name, -position, -adp),
+      by = 'sleeper_id'
+    )
+}
+
+df_ffc <- get_ffc_adp('ppr', '2024') |> 
+  clean_ffc_adp(ff_ids)
+
 draft_picks <- get_draft_picks()
-df_base <- get_base_table(clean_fantasy_life_adp('~/Downloads/nfl_adp.csv', 2024), draft_picks)
-df_base
+df_fantasy_life_adp <- clean_fantasy_life_adp('~/Downloads/nfl_adp.csv', 2024)
+df_adp <- combine_ffc_fantasy_life_adp(df_ffc, df_fantasy_life_adp)
+
+
+
+
+df_adp
 
 
 df_base <- df_base |> mutate(drafted = ifelse(merge_name %in% c('ceedee lamb', 'tyreek hill', 'christian mccaffrey'), FALSE, drafted))
